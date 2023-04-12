7e38b1ab0220e95c07ee7cead85dc0dbbaf53171
Jhon Sebastian Navas Chafuelan <jsnavas@cenicana.org>
2023-04-12 14:03:14 -0500

    V1

---
+++ src/CreateTodoButton/index.js
@@ -0,0 +1,21 @@
+import react from "react";
+import './createTodoButton.css'
+import React from 'react';
+import './createTodoButton.css';
+
+function CreateTodoButton(props) {
+  const onClickButton = () => {
+    props.setOpenModal(prevState => !prevState);
+  };
+
+  return (
+    <button
+      className="CreateTodoButton"
+      onClick={onClickButton}
+    >
+      +
+    </button>
+  );
+}
+
+export { CreateTodoButton };
